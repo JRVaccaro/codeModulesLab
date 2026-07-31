ddc56456c40eeea61a8f8dbfcc112d8596a6ae28
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-31 14:00:19 -0400

    File set up for cat adoption tracker project

---
+++ Cat-Adoption-Tracker/cats.js
@@ -0,0 +1,16 @@
+
+const cats = [
+    {
+        name: "Pepper",
+        breed: "Domestic Short hair",
+        age: 1,
+        adopted: false
+    },
+{
+        name: "Big Boy",
+        breed: "Domestic Short Hair",
+        age:  4,
+        adopted: false
+    }
+
+];
